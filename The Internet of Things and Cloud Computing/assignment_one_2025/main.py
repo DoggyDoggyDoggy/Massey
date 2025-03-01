5a7c93794079c6bc3d6367389c5f1b61c3e982bc
DoggyDoggyDoggy <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-03-01 21:55:38 +1300

    Draft
    Example from Massey

---
+++ The Internet of Things and Cloud Computing/assignment_one_2025/main.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify, send_from_directory
+from sense_emu import SenseHat
+import os
+
+app = Flask(__name__)
+sense = SenseHat()
+
+@app.route('/temperature')
+def get_temperature():
+    try:
+        temperature = sense.get_temperature()
+        warnings = []
+
+        if not (0 <= temperature <= 50):
+            raise ValueError("Received implausible data values.")
+
+        if temperature > 40:
+            warnings.append("High Temp!")
+            sense.show_message("High Temp!", text_colour=[255, 0, 0])
+
+        return jsonify(temperature=temperature, warnings=warnings)
+    except Exception as e:
+        return jsonify(error=str(e)), 500
+
+@app.route('/')
+def home():
+    return send_from_directory(os.getcwd(), 'HTMLEmuData.html')
+
+if __name__ == '__main__':
+    app.run(debug = True)
+
