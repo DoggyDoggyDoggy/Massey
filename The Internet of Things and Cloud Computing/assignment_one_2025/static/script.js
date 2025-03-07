c38d9d36cb8a64eb9c8402ddac1fd3ad16c4ecff
DoggyDoggyDoggy <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-03-07 16:11:32 +1300

    Comments for HTML + scripts

--- The Internet of Things and Cloud Computing/assignment_one_2025/static/script.js
+++ The Internet of Things and Cloud Computing/assignment_one_2025/static/script.js
@@ -1,3 +1,9 @@
+//This script file were taken from the example that was shown in the second week
+// I expanded it to show humidity and air pressure.
+// The logic for displaying and deleting warnings was also reworked.
+//Now warnings are displayed for 10 seconds and are deleted by themselves.
+//This was done to avoid a bug with flickering when parsing a new date.
+// The trend variable has been added
 function updateTemperatureDisplay(data) {
   const tempDisplay = document.getElementById('temperature');
   const humidityDisplay = document.getElementById('humidity');
@@ -42,8 +48,8 @@ function fetchTemperatureData() {
     });
 }
 
-// Execute fetchTemperatureData when the page loads and set it to repeat every 5 seconds
+// Execute fetchTemperatureData when the page loads and set it to repeat every 1 seconds
 window.onload = function() {
   fetchTemperatureData(); // Fetch temperature data immediately on page load
-  setInterval(fetchTemperatureData, 5000); // Continuously fetch data every 5 seconds
+  setInterval(fetchTemperatureData, 1000); // Continuously fetch data every 1 seconds
 };
