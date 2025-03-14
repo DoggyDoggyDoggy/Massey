cb9b6c6aa81f85582a25ab7cfe8b1363fa3d47a4
DoggyDoggyDoggy <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-03-15 01:24:56 +1300

    Utils folder

--- Programming Languages, Algorithms and Concurrency/Assignment_one/src/Main.kt
+++ Programming Languages, Algorithms and Concurrency/Assignment_one/src/Main.kt
@@ -1,15 +1,5 @@
-fun printHeader() {
-    println("----------------------------------------")
-    println("159.341 Assignment 1 Semester 1 2025")
-    println("Submitted by: Denys Pedan, 23011350")
-    println("----------------------------------------")
-}
+import utils.printHeader
 
-// A function for extracting words from a string using a given pattern.
-fun extractWords(text: String): List<String> {
-    val regex = Regex("[A-Za-z0-9]+(?:['-][A-Za-z0-9]+)*")
-    return regex.findAll(text).map { it.value }.toList()
-}
 
 fun main() {
     printHeader()
---
+++ Programming Languages, Algorithms and Concurrency/Assignment_one/src/utils/Utils.kt
@@ -0,0 +1,14 @@
+package utils
+
+fun printHeader() {
+    println("----------------------------------------")
+    println("159.341 Assignment 1 Semester 1 2025")
+    println("Submitted by: Denys Pedan, 23011350")
+    println("----------------------------------------")
+}
+
+// A function for extracting words from a string using a given pattern.
+fun extractWords(text: String): List<String> {
+    val regex = Regex("[A-Za-z0-9]+(?:['-][A-Za-z0-9]+)*")
+    return regex.findAll(text).map { it.value }.toList()
+}
