fb783afcc03b4e26d4d4b8d5d929a0d33b4c3781
DoggyDoggyDoggy <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-03-15 01:06:38 +1300

    Header

--- Programming Languages, Algorithms and Concurrency/Assignment_one/src/Main.kt
+++ Programming Languages, Algorithms and Concurrency/Assignment_one/src/Main.kt
@@ -1,3 +1,10 @@
-fun main() {
+fun printHeader() {
+    println("----------------------------------------")
+    println("159.341 Assignment 1 Semester 1 2025")
+    println("Submitted by: Denys Pedan, 23011350")
+    println("----------------------------------------")
+}
 
+fun main() {
+    printHeader()
 }
