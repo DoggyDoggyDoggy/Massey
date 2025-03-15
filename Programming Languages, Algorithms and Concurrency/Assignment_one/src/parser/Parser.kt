fa674781e8dc231a7b32a33fb4fae576367dd390
DoggyDoggyDoggy <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-03-16 00:03:14 +1300

    Parser draft

--- Programming Languages, Algorithms and Concurrency/Assignment_one/src/parser/Parser.kt
+++ Programming Languages, Algorithms and Concurrency/Assignment_one/src/parser/Parser.kt
@@ -2,6 +2,59 @@ package parser
 
 import lexer.Token
 
-class Parser (val tokens: List<Token>) {
+class Parser(val tokens: List<Token>) {
+    var pos = 0
 
+    fun current(): Token? = if (pos < tokens.size) tokens[pos] else null
+
+    fun eat(expectedType: String): Token {
+        val token = current() ?: throw Exception("Expected token $expectedType, but end of input reached.")
+        if (token.type != expectedType)
+            throw Exception("Expected token $expectedType, and ${token.type} (${token.value}) is received at ${token.pos}")
+        pos++
+        return token
+    }
+
+    fun parseStatement() {
+        val token = current() ?: throw Exception("No input data")
+        return when (token.type) {
+            "APPEND" -> {
+
+            }
+
+            "SET" -> {
+
+            }
+
+            "PRINT" -> {
+
+            }
+
+            "PRINTLENGTH" -> {
+
+            }
+
+            "PRINTWORDS" -> {
+
+            }
+
+            "PRINTWORDCOUNT" -> {
+
+            }
+
+            "LIST" -> {
+
+            }
+
+            "REVERSE" -> {
+
+            }
+
+            "EXIT" -> {
+
+            }
+
+            else -> throw Exception("Unknown instruction starting with token ${token.type}")
+        }
+    }
 }
