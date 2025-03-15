24e6480b5864377251a1998d7832ac491ee0ceb3
DoggyDoggyDoggy <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-03-15 16:26:55 +1300

    User input

--- Programming Languages, Algorithms and Concurrency/Assignment_one/src/Main.kt
+++ Programming Languages, Algorithms and Concurrency/Assignment_one/src/Main.kt
@@ -3,4 +3,21 @@ import utils.printHeader
 
 fun main() {
     printHeader()
+    val env = mutableMapOf<String, String>()  // Symbol table
+    var buffer = ""
+    var prompt = "> "
+    while (true) {
+        print(prompt)
+        val line = readLine() ?: break
+        buffer += line + "\n"
+        while (buffer.contains(";")) {
+            val index = buffer.indexOf(";")
+            var stmtText = buffer.substring(0, index).trim()
+            buffer = buffer.substring(index + 1)
+            if (stmtText.isEmpty()) continue
+            stmtText += ";"
+            println(stmtText)
+        }
+        break
+    }
 }
