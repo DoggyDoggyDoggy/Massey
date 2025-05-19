040142344a2beb78d73d790a4d69132e0d15aadd
Denys <pedan.denys@gmail.com>
2025-05-19 17:03:54 +1200

    test version of write data to DynamoDB

---
+++ The Internet of Things and Cloud Computing/assignment_two/raspberry pi/aws_db.py
@@ -0,0 +1,15 @@
+import boto3
+from datetime import datetime
+
+dynamodb = boto3.resource('dynamodb', region_name='ap-southeast-2')
+table = dynamodb.Table('SoilMoisture')
+
+def put_to_dynamo(device_id):
+    timestamp = int(datetime.utcnow().timestamp())
+    table.put_item(Item={
+        'deviceId': device_id,
+        'timestamp': timestamp
+    })
+
+put_to_dynamo("py_test")
+print("done")
