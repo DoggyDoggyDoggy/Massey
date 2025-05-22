a9ef422fdc3193eae52b035e0543544f4e18bc4b
Denys <pedan.denys@gmail.com>
2025-05-23 00:59:38 +1200

    Read data from arduino any time

--- The Internet of Things and Cloud Computing/assignment_two/sketch_may16a/sketch_may16a.ino
+++ The Internet of Things and Cloud Computing/assignment_two/sketch_may16a/sketch_may16a.ino
@@ -1,8 +1,26 @@
+const unsigned long INTERVAL = 30UL * 60UL * 1000UL; // 30 minutes
+unsigned long previousMillis = 0;
+
 void setup() {
   Serial.begin(9600);
+  while (!Serial) ;
 }
 
 void loop() {
-  Serial.print(analogRead(0)); 
-  delay(1800000);
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= INTERVAL) {
+    previousMillis = currentMillis;
+    int moisture = analogRead(A0);
+    Serial.println(moisture);
+  }
+
+  if (Serial.available()) {
+    String cmd = Serial.readStringUntil('\n');
+    cmd.trim();
+    if (cmd == "read") {
+      int moisture = analogRead(A0);
+      Serial.println(moisture);
+    }
+  }
 }
